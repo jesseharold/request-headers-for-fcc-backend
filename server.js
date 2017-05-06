a29f1efb10b86b0cf1c717479f7da5b81deded53
Harold <jesseharold@gmail.com>
2017-05-06 01:08:01 +0000

    basic project complete

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+app.get("/", function(req, res){
+    var responseHTML = "<html><body>";
+    responseHTML += "ip address: " + req.headers["x-forwarded-for"];
+    responseHTML += "<br><br>";
+    responseHTML += "language: " + req.headers["accept-language"].slice(0, req.headers["accept-language"].indexOf(","));
+    responseHTML += "<br><br>";
+    responseHTML += "operating system: " + req.headers["user-agent"].slice(req.headers["user-agent"].indexOf("(")+1, req.headers["user-agent"].indexOf(")"));
+    responseHTML += "<br><br>";
+    responseHTML += "</body></html>";
+    res.send(responseHTML);
+})
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
